2fb1f43007cc309781fbb7edbd28dde88ec3e101
EvgenBatt <evgenmgid@gmail.com>
2023-09-14 12:48:12 +0300

    Add slick slider

---
+++ js/about-slider.js
@@ -0,0 +1,42 @@
+/* About Slide */
+$(".about-slide").slick({
+  dots: false,
+  infinite: true,
+  centerMode: true,
+  variableWidth: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  edgeFriction: 0.5,
+  arrows: false,
+  responsive: [
+    {
+      breakpoint: 1440,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 375,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+  ],
+});
+
+$(".about-slide-prev").on("click", function () {
+  $(".about-slide").slick("slickPrev");
+});
+
+$(".about-slide-next").on("click", function () {
+  $(".about-slide").slick("slickNext");
+});
